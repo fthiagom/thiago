aac0b81d593cd088052041b6d833e032ea3d1598
thiago martins <59349734+fthiagom@users.noreply.github.com>
2022-11-20 21:24:51 -0300

    calc 1

---
+++ contas simples 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conta_simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());  
+            int B = int.Parse(Console.ReadLine());
+            int SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
