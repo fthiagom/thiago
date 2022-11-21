527a8299a47e9371c122ddd0761f4ce56fc4e693
thiago martins <59349734+fthiagom@users.noreply.github.com>
2022-11-20 23:02:12 -0300

    beecrowd | 1005

---
+++ media 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_com_peso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Nota1 = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double Nota2 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double MEDIA = (Nota1 * 3.5 + Nota2 * 7.5) / 11;
+          
+            Console.WriteLine($"MEDIA = { MEDIA.ToString("f5", CultureInfo.InvariantCulture)}");
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ media 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_com_peso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double B = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double C = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double MEDIA = (A * 2 + B * 3 + C*5) / 10;
+          
+            Console.WriteLine($"MEDIA = { MEDIA.ToString("f1", CultureInfo.InvariantCulture)}");
+            Console.ReadLine();
+
+        }
+        
+    }
+}
