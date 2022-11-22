4e29a864131e82132274fe3126e27367e1e1bafe
thiago martins <59349734+fthiagom@users.noreply.github.com>
2022-11-21 23:51:12 -0300

    beecrowd

---
+++ salario com bonus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salario_com_bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string nome = Console.ReadLine();
+            double salfix = Convert.ToDouble(Console.ReadLine());
+            double vendas = Convert.ToDouble(Console.ReadLine());
+            double resultado = (salfix + vendas * 0.15);
+
+            Console.WriteLine($"TOTAL = R$ {resultado:F2}");
+            Console.ReadLine();
+
+        }
+    }
+}
