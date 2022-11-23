402190ef4573c1c34fbe56be408432a62abc63b4
thiago martins <59349734+fthiagom@users.noreply.github.com>
2022-11-22 22:12:06 -0300

    beecrowd

---
+++ o maior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o_maior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dist = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(dist *2 + " minutos");
+
+            Console.ReadLine();
+        }
+    }
+}
