5cbc0563f4efdfc3d23c68c032d25773381b78da
thiago martins <59349734+fthiagom@users.noreply.github.com>
2022-11-22 23:30:47 -0300

    beecrowd

---
+++ conversor de horras.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beecrowd1016Distancia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            int hora = x / 3600;    //38 horas. vai sobrar 00,93//
+            int restohora = x % 3600; //00,93//
+            int somamin = restohora / 60;//somamin é 1//
+            int restosomamin = somamin % 60;//55 segundos
+            int restomin = x % 60;
+           
+
+
+
+            Console.WriteLine($"{hora}:{somamin}:{restomin}");
+            Console.ReadKey();
+        }
+    }
+}
