49ec9d9a07cd70934844f0112147e7e817cfb2ad
thiago martins <59349734+fthiagom@users.noreply.github.com>
2022-11-22 22:03:13 -0300

    beecrowd

---
+++ consumo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consulmo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = Convert.ToInt32(Console.ReadLine());
+            double Y = Convert.ToDouble(Console.ReadLine());
+            double result = X / Y;
+
+            Console.WriteLine($"{result:F3} km/l");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ gasto de combustivel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd1017ConsumoDeCombustivel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int tempo, velmed, dist;
+            double conslit = 12.000;
+            double constot;
+
+            tempo = Convert.ToInt32(Console.ReadLine());
+            velmed = Convert.ToInt32(Console.ReadLine());
+            dist = tempo * velmed;
+            constot = dist / conslit;
+
+            Console.WriteLine($"{constot:F3}");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
