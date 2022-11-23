abf8486a4104b1822fa7c43f785c968536857643
thiago martins <59349734+fthiagom@users.noreply.github.com>
2022-11-23 18:02:48 -0300

    beecrowd

---
+++ Idade em Dias.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Beecrowd1016Distancia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            int ano = x / 365;
+            int resto = x % 365;
+
+            int mes = resto / 30;
+            int resto2 = resto % 30;
+
+            int dia = resto2;
+  
+            
+
+            Console.WriteLine(ano+ " ano(s)");
+            Console.WriteLine(mes+ " mes(es)");
+            Console.WriteLine(dia+ " dia(s)");
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
