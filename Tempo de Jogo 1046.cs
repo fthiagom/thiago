447d4ecae2f09a81ccfb5e00dd2a9d9922d3f2ae
thiago martins <fthiagomartins@gmail.com>
2022-12-04 19:18:05 -0300

    beecrowd

---
+++ Tempo de Jogo 1046.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Beecrowd1046Tempodejogo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] horas = Console.ReadLine().Split();
+            int inicio = int.Parse(horas[0]);
+            int fim = int.Parse(horas[1]);
+            int tempo;
+
+            if (inicio > fim)
+            {
+                tempo = (24 - inicio) + (0 + fim);
+                Console.WriteLine($"O JOGO DUROU {tempo} HORA(S)");
+
+            }
+            else if (inicio == fim)
+            {
+                Console.WriteLine($"O JOGO DUROU 24 HORA(S)");
+
+            }
+            else if (inicio < fim)
+            {
+                tempo = fim - inicio;
+                Console.WriteLine($"O JOGO DUROU {tempo} HORA(S)");
+
+            }
+
+
+
+        }
+    }
+}
