8e6b5f75bf94ff3790494af2f56bdccadf1e08fb
thiago martins <fthiagomartins@gmail.com>
2022-12-09 22:46:14 -0300

    beecrowd

---
+++ Números Pares 1059.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
