0603c2154d586836d3bd55fbee6415805c617c98
thiago martins <fthiagomartins@gmail.com>
2022-12-12 15:17:23 -0300

    1072  Intervalo 2

---
+++ 1072 Intervalo 2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace becrowd1072
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int totaldenumero = int.Parse(Console.ReadLine());
+            int[] inteiros = new int[totaldenumero];
+            int inf = 0;
+            int fe = 0;
+
+            for (int i = 0; i < inteiros.Length; i++)
+            {
+                inteiros[i] = int.Parse(Console.ReadLine());
+            }
+
+           foreach (int numero in inteiros)
+            {
+
+                if (numero <= 20 && numero >= 10)
+                {
+                    inf++;
+                }
+                else if (numero > 20 || numero < 10)
+                {
+                    fe++;
+                }
+            
+            }
+            Console.WriteLine(inf + " in");
+            Console.WriteLine(fe +" out");
+            Console.ReadKey();
+        }
+    }
+}
