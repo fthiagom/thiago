634dce91582ceb0917fbd495360f31c6c0eb1ead
thiago martins <fthiagomartins@gmail.com>
2022-12-12 18:40:13 -0300

    beecrowd

---
+++ 1075 Resto 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = int.Parse(Console.ReadLine());
+            foreach (int value in Enumerable.Range(1, 10001).Where(x => x % val == 2))
+            {
+                Console.WriteLine(value);
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
