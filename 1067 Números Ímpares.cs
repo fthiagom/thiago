e31d68276d2aa584e8892b9459294f5d334ed648
thiago martins <fthiagomartins@gmail.com>
2022-12-12 11:04:21 -0300

    beecrowd

--- 1067 Números Ímpares.cs
+++ 1067 Números Ímpares.cs
@@ -11,54 +11,16 @@ namespace numeros_pares
     {
         static void Main(string[] args)
         {
-            int vpares = 0;
-            int vimp = 0;
-            int pos = 0;
-            int neg = 0;
+            int valor = int.Parse(Console.ReadLine());
 
-            int valor1 = int.Parse(Console.ReadLine());
-            int valor2 = int.Parse(Console.ReadLine());
-            int valor3 = int.Parse(Console.ReadLine());
-            int valor4 = int.Parse(Console.ReadLine());
-            int valor5 = int.Parse(Console.ReadLine());
-
-            int[] valores = new int[5];
-
-            valores[0] = valor1;
-            valores[1] = valor2;
-            valores[2] = valor3;
-            valores[3] = valor4;
-            valores[4] = valor5;
-            
-            for (int i = 0; i < valores.Length; i ++)
+            for (int i = 0; i <= valor; i += 1)
             {
-                double resto = valores[i] % 2;
-                if(resto == 0)
+                double resto = i % 2;
+                if (resto != 0)
                 {
-                    vpares++;
-                }
-                else if (resto != 0) 
-                {
-                    vimp++;
-                }
-
-            }
-            for (int i = 0; i < valores.Length; i++) 
-            {
-                if (valores[i] > 0)
-                {
-                    pos++;
-                }
-                else if (valores[i] < 0) 
-                {
-                    neg++;
+                    Console.WriteLine(i);
                 }
             }
-
-            Console.WriteLine(vpares + " valor(es) par(es)");
-            Console.WriteLine(vimp + " valor(es) impar(es)");
-            Console.WriteLine(pos + " valor(es) positivo(s)");
-            Console.WriteLine(neg + " valor(es) negativo(s)");
             Console.ReadKey();
 
         }
