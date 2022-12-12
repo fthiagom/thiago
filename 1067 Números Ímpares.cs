626b85f064a1be97970303aa4d4ebe3e87e21c6b
thiago martins <fthiagomartins@gmail.com>
2022-12-12 10:41:06 -0300

    beecrowd

---
+++ 1067 Números Ímpares.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= valor; i += 1)
+            {
+                double resto = i % 2;
+                if (resto != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
