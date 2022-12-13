9513439d075b5bc4525feb8a86bf0323585b555c
thiago martins <fthiagomartins@gmail.com>
2022-12-13 15:17:32 -0300

    beecrowd

---
+++ 1095 Sequencia IJ 1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace becrowd1095
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int b = 65;
+
+            for (int i = 1; i <= 37; i += 3)
+            {
+
+                b = b - 5;
+
+                Console.WriteLine("I=" + i + " J=" + b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1096 Sequencia IJ 2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace becrowd1095
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2;
+            for (num1 = 1; num1 <= 9; num1 += 2)
+            {
+                for (num2 = 7; num2 >= 5; num2--)
+                {
+                  Console.WriteLine("I= "+num1+ " J="+ num2);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
