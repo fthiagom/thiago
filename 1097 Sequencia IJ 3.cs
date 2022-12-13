c2e2ba3a4c9ba1391e38d3f24b344378e9c759eb
thiago martins <fthiagomartins@gmail.com>
2022-12-13 17:30:07 -0300

    beecrowd

---
+++ 1097 Sequencia IJ 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace becrowd1095
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = 1, cont = 7;
+            while (num <= 9)
+            {
+                Console.WriteLine("I="+num+ " J="+cont--);
+                Console.WriteLine("I=" + num + " J=" + cont--);
+                Console.WriteLine("I=" + num + " J=" + cont--);
+                num += 2;
+                cont += 5;
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
